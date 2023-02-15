7c13811c2e7abfb3727e52444738d35438100960
Drake Wright <rdrakewright@gmail.com>
2023-02-15 08:26:37 -0600

    Initial Commit

---
+++ State Exercise/State_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_ExerciseApp.swift
+//  State Exercise
+//
+//  Created by Drake Wright on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct State_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
